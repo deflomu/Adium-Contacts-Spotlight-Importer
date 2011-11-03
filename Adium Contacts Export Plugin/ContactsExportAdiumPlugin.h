807d317aa2575eaf78570c3d9966a8528d0764fe
elm <elm@skweez.net>
2011-11-03 18:07:21 +0100

    inital version of plugin

---
+++ Adium Contacts Export Plugin/ContactsExportAdiumPlugin.h
@@ -0,0 +1,14 @@
+//
+//  ContactsExportAdiumPlugin.h
+//  Adium Contacts Spotlight Importer
+//
+//  Created by Florian Mutter on 03.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Adium/AIPlugin.h>
+
+@interface ContactsExportAdiumPlugin : NSObject <AIPlugin>
+
+@end
---
+++ Adium Contacts Export Plugin/ContactsExportAdiumPlugin.m
@@ -0,0 +1,23 @@
+//
+//  ContactsExportAdiumPlugin.m
+//  Adium Contacts Spotlight Importer
+//
+//  Created by Florian Mutter on 03.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ContactsExportAdiumPlugin.h"
+
+@implementation ContactsExportAdiumPlugin
+
+- (void) installPlugin
+{
+    [adium contactController] 
+}
+
+- (void) uninstallPlugin
+{
+    
+}
+
+@end
